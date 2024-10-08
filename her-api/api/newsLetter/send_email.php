7f2c837b1fc6f92ddcb8665e73c8ccbe540beda0
Kwayu Mmari <mmaridenis@gmail.com>
2024-10-08 17:13:47 +0300

    deleted page module.css

---
+++ her-api/api/newsLetter/send_email.php
@@ -0,0 +1,24 @@
+<?php
+// send_email.php
+
+// Get POST data from the request
+$data = json_decode(file_get_contents('php://input'), true);
+
+if (isset($data['message']) && isset($data['to'])) {
+    $to = $data['to']; // Recipient's email address
+    $subject = "New Message"; // Subject of the email
+    $message = $data['message']; // Email message
+    $headers = "From: info@herinitiative.or.tz" . "\r\n" . // Sender's email
+               "Reply-To: info@herinitiative.or.tz" . "\r\n" . // Reply-to email
+               "X-Mailer: PHP/" . phpversion(); // PHP version
+
+    // Sending the email
+    if (mail($to, $subject, $message, $headers)) {
+        echo json_encode(['status' => 'success', 'message' => 'Email sent successfully!']);
+    } else {
+        echo json_encode(['status' => 'error', 'message' => 'Failed to send email.']);
+    }
+} else {
+    echo json_encode(['status' => 'error', 'message' => 'Invalid data provided.']);
+}
+?>
