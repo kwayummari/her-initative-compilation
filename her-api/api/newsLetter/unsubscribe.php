1d8692f4e8af98eec4e575b6982d2a3b60ce9742
Kwayu Mmari <mmaridenis@gmail.com>
2024-10-08 18:54:18 +0300

    deleted page module.css

---
+++ her-api/api/newsLetter/unsubscribe.php
@@ -0,0 +1,32 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json');
+include('../../config/db.php');
+
+class API {
+    private $db;
+
+    public function __construct() {
+        $this->db = new Connect;
+    }
+
+    public function Delete($email) {
+        $query = $this->db->prepare("DELETE FROM newsLetter WHERE email = :email"); // Fixed the SQL syntax
+        $query->bindParam(':email', $email, PDO::PARAM_INT);
+
+        if($query->execute()) {
+            echo 'You have successfully unsubscribed from our newsletter. We’re sorry to see you go, but you’ll no longer receive updates from us. If this was a mistake, feel free to subscribe again anytime!';
+        } else {
+            return json_encode(array("message" => "Error deleting blog content."));
+        }
+    }
+}
+
+$API = new API();
+
+if(isset($_POST['id'])) {
+    $email = intval($_GET['email']); // Ensure the ID is an integer
+
+    echo $API->Delete($email);
+}
+?>
